9c5ef3cda827d82bbe19793b31100c236269ca73
Tofik Mohammed <tofikmohammed@Tofiks-MacBook-Air.local>
2021-12-02 03:40:49 -0500

    refactoring

--- wordsearch.js
+++ wordsearch.js
@@ -16,30 +16,20 @@ const transpose = function (matrix) {
 const wordSearch = function (letters, word) {
   let newletters = [];
   let newTransposedLetters = [];
+
   for (i = 0; i < letters.length; i++) {
     newletters.push(letters[i].join(""));
   }
 
-  for (newletter of newletters) {
-    if (newletter === word) {
-      return true;
+  if (newletters.includes(word)) return true;
+  else {
+    letters = transpose(letters);
+    for (i = 0; i < letters.length; i++) {
+      newTransposedLetters.push(letters[i].join(""));
     }
+    if (newTransposedLetters.includes(word)) return true;
   }
-
-  letters = transpose(letters);
-  for (i = 0; i < letters.length; i++) {
-    newTransposedLetters.push(letters[i].join(""));
-  }
-
-  for (newTransposedLetter of newTransposedLetters) {
-    if (newTransposedLetter === word) {
-      return true;
-    }
-  }
-
-  return false
+  return false;
 };
-
-
 
 module.exports = wordSearch;
