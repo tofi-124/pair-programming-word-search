c058d42b8304ce79a4cceb743418556c52492b49
Tofik Mohammed <tofikahm@yahoo.com>
2021-12-02 07:10:46 +0000

    Word Search working

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,45 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function (matrix) {
+  let resultingArray = [];
+  const nrOfRows = matrix[0].length;
+  const nrOfColumns = matrix.length;
 
-module.exports = wordSearch
+  for (let i = 0; i < nrOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < nrOfColumns; j++) {
+      row.push(matrix[j][i]);
+    }
+    resultingArray.push(row);
+  }
+  return resultingArray;
+};
+
+const wordSearch = function (letters, word) {
+  let newletters = [];
+  let newTransposedLetters = [];
+  for (i = 0; i < letters.length; i++) {
+    newletters.push(letters[i].join(""));
+  }
+
+  for (newletter of newletters) {
+    if (newletter === word) {
+      return true;
+    }
+  }
+
+  letters = transpose(letters);
+  for (i = 0; i < letters.length; i++) {
+    newTransposedLetters.push(letters[i].join(""));
+  }
+
+  for (newTransposedLetter of newTransposedLetters) {
+    if (newTransposedLetter === word) {
+      return true;
+    }
+  }
+
+  return false
+};
+
+
+
+module.exports = wordSearch;
